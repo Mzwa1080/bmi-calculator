8957a463b28376c5ea2d7e33027741c3d3874b2e
Mzwa1080 <mshawnlouw@gmail.com>
2023-11-13 04:24:25 +0200

    I need to check the weight and height if entered on not

--- script/index.js
+++ script/index.js
@@ -1,12 +1,12 @@
 function calculateBMI() {
-    var weight = document.getElementById("weight").value;
-    var height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
 
     if (weight && height && height <=3 ) {
 
-        var bmi = weight / (height * height);
+        let bmi = weight / (height * height);
 
-        document.getElementById("bmiValue").textContent = bmi.toFixed(2);
+        document.getElementById("bmiValue").textContent = bmi;
         document.getElementById("result").style.display = "block";
     } else {
         // If weight or height is not provided, display an error message
