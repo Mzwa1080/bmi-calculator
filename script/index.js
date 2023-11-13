2f7286ec5733ac471682b92f1f1bf43fc9c0750b
Mzwa1080 <mshawnlouw@gmail.com>
2023-11-13 04:41:21 +0200

    I checked if the inputs are empty then perfom specific conditions

--- script/index.js
+++ script/index.js
@@ -1,15 +1,18 @@
+let btn = document.querySelector('[data-submit]');
+
 function calculateBMI() {
     let weight = document.getElementById("weight").value;
     let height = document.getElementById("height").value;
 
-    if (weight && height && height <=3 ) {
+    if (weight !== '' && height !=='' && height <=3 ) {
 
         let bmi = weight / (height * height);
 
         document.getElementById("bmiValue").textContent = bmi;
-        document.getElementById("result").style.display = "block";
     } else {
         // If weight or height is not provided, display an error message
         alert("Please enter both weight and a correct height to calculate BMI.");
     }
 }
+
+btn.addEventListener('click', calculateBMI)
