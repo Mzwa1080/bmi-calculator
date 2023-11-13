3850cd5b5c21cec65252185945c1da1ea00faf9a
Mzwa1080 <mshawnlouw@gmail.com>
2023-11-13 14:40:06 +0200

    Never been this tired but I enjoyed

--- script/index.js
+++ script/index.js
@@ -1,17 +1,30 @@
 let btn = document.querySelector('[data-submit]');
+let displayResults =  document.getElementById('bmiValue');
 
 function calculateBMI() {
     let weight = document.getElementById("weight").value;
     let height = document.getElementById("height").value;
 
-    if (weight !== '' && height !=='' && height <=3 ) {
+    if (weight !== '' && height !=='' ) {
 
-        let bmi = weight / (height * height);
+        let bmi = (weight / (height * height)) * 10000;
+        // I must convert the decimals in to two decimal place
+        bmi = bmi.toFixed(2);
+        if(bmi < 18.5){
+            displayResults.textContent = `${bmi}. This is described as Underweight!`;
+        } else if( 18.5 >= bmi <=24.9 ){
+            displayResults.textContent = `${bmi}. This is described as Healthy range!`;
+        } else if(25>= bmi <=29.9 ){
+            displayResults.textContent = `${bmi}. This described as Overweight!`
+        }else if(30>= bmi <=39.9){
+            displayResults.textContent = `${bmi}. This is described as Obesity!`
+        }else if(bmi>40){
+            displayResults.textContent = `${bmi}. This is described as severe obesity!`
+        }
 
-        document.getElementById("bmiValue").textContent = bmi;
     } else {
         // If weight or height is not provided, display an error message
-        alert("Please enter both weight and a correct height to calculate BMI.");
+        alert('Please enter a your weight or height!')
     }
 }
 
